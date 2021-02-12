e60f93ef050884fe81f76aa219d063f929def2fd
Musa Kuşbey <78588618+Musakusbey@users.noreply.github.com>
2021-02-12 15:13:51 +0300

    Add files via upload
    Nesne Yönelimli Programlama (Object-Oriented Programming)---1.Sınıf Tanımlama

---
+++ 1.Sınıf tanımlama.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Nokta
+{
+	public:
+		
+		int x,y;
+};
+
+int main ()
+{
+	Nokta k;
+	
+	k.x=10;
+	
+	k.y=20;
+	
+	cout<<k.x<<","<<k.y<<endl;
+	
+	return 0;
+}
